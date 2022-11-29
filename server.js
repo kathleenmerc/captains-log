97ed844ff6ef1b7e3d031fdb0c522822b89d332b
Kathleen Mercado <kathleenmerc@gmail.com>
2022-11-29 16:20:49 -0500

    added middleware, created Create route

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const reactViews = require('express-react-views')
 app.set("view engine", "jsx")
 app.engine("jsx", reactViews.createEngine())
 
+// MIDDLEWARE
+app.use((req, res, next) => {
+    console.log("Middleware is running for all routes")
+    next()
+})
+
+app.use(express.urlencoded({extended:false}))
+
+// CREATE
+app.post('/logs', (req, res) => {
+    res.send(req.body)
+})
+
 // NEW
 app.get('/logs/new', (req, res) => {
     res.render('New')
